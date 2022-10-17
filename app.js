048c7c42379fb7e755e91886232361546697ddb1
jongin12 <jongin981224@gmail.com>
2022-10-17 11:23:15 +0900

    math.js 함수 추가

--- app.js
+++ app.js
@@ -107,34 +107,40 @@ app.get("/summoner/:name/activegame", async function (req, res) {
 });
 
 app.get("/match/:id", async function (req, res) {
-  let list = { user: [] };
+  let list = { user: [], math: {} };
   list.match = await LOL_API.matchInfo(req.params.id, "name");
+  list.timeLine = await LOL_API.timeLine(req.params.id);
   for (let i = 0; i < 10; i++) {
     let userId = list.match.userinfo[i].summonerId;
     list.user[i] = await LOL_API.summonersLeague(userId);
   }
-  // res.send(list);
-  new Promise((resolve, reject) => {
-    if (list.status) {
-      reject("없는 matchId입니다");
-    } else {
-      resolve();
-    }
-  })
-    .then(() => {
-      fs.readFile("html/match-info.ejs", "utf8", function (err, data) {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(
-          ejs.render(data, {
-            matchData: list.match,
-            userData: list.user,
-          })
-        );
-      });
-    })
-    .catch((err) => {
-      res.send(err);
-    });
+  list.math.deal_15min = math.deal_15min(list.timeLine.info.frames);
+  list.math.eliteMonster_Kill = math.eliteMonster_Kill(
+    list.timeLine.info.frames
+  );
+  list.math.goldDifference = math.goldDifference(list.timeLine.info.frames);
+  res.send(list);
+  // new Promise((resolve, reject) => {
+  //   if (list.status) {
+  //     reject("없는 matchId입니다");
+  //   } else {
+  //     resolve();
+  //   }
+  // })
+  //   .then(() => {
+  //     fs.readFile("html/match-info.ejs", "utf8", function (err, data) {
+  //       res.writeHead(200, { "Content-Type": "text/html" });
+  //       res.end(
+  //         ejs.render(data, {
+  //           matchData: list.match,
+  //           userData: list.user,
+  //         })
+  //       );
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     res.send(err);
+  //   });
 });
 
 app.listen(8080, function () {
--- math.js
+++ math.js
@@ -1,3 +1,5 @@
+const timestamp = require("./timestamp.json");
+
 const math = {
   matchData: (matchData) => {
     let data_num = matchData.length;
@@ -12,22 +14,51 @@ const math = {
     arr.matchData = data_num;
     arr.win = win;
     return arr;
+    // 총 경기 수와 그중 승리 수를 객체로 반환
   },
   goldDifference: (frames) => {
     let value = [];
-    frames.foreach((item) => {
+    frames.forEach((item) => {
       var team1 = 0;
-      for (let i = 0; i < 5; i++) {
-        team1 = +item.participantFrames[i].totalGold;
+      for (let i = 1; i < 6; i++) {
+        team1 += item.participantFrames[i].totalGold;
       }
       var team2 = 0;
-      for (let i = 5; i < 10; i++) {
-        team2 = +item.participantFrames[i].totalGold;
+      for (let i = 6; i < 11; i++) {
+        team2 += item.participantFrames[i].totalGold;
       }
       value.push(team1 - team2);
     });
     return value;
+    //1분당 골드차이 배열로 반환
+  },
+  deal_15min: (frames) => {
+    let value = [];
+    let arr = frames[15].participantFrames;
+    for (key in arr) {
+      let dmg = arr[key].damageStats.totalDamageDoneToChampions;
+      value.push(dmg);
+    }
+    return value;
+    //유저별로 15분 시점의 딜량 배열로 반환
+  },
+  eliteMonster_Kill: (frames) => {
+    value = [];
+    frames.forEach((item) => {
+      let events = item.events;
+      events.forEach((event) => {
+        if (event.type === "ELITE_MONSTER_KILL") {
+          value.push(event);
+        }
+      });
+    });
+    return value;
+    //엘리트몬스터 처치로그 배열로 반환
   },
 };
+
+console.log(math.goldDifference(timestamp.info.frames));
+// console.log(math.deal_15min(timestamp.info.frames));
+// console.log(math.eliteMonster_Kill(timestamp.info.frames));
 
 module.exports = math;
